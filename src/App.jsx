85eeccef12f52da29acca0cea4827d9d56851072
Filip Tomashevski <93556752+FilipTomash@users.noreply.github.com>
2024-03-11 14:49:41 +0100

    intial commit for todo app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos:</h1>
+    </>
+  );
+}
+
+export default App;
